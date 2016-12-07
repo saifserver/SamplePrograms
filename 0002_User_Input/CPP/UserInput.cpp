ba1a6b2f7b4c272c16b8655563864dc6be7f3b80
saifylovezyou <saifuddin.ksyed@gmail.com>
2016-12-07 16:05:40 +0530

    Commit

---
+++ 0002_User_Input/CPP/UserInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+  string name;
+  cout << "What's your name?";
+  getline(cin, name);
+  cout << "Hello " << name << ".\n";
+  return 0;
+}
