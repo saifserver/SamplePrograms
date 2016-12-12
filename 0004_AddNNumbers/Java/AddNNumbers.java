e3d0fb85496cd64d4a73172b550cbb3a8ad43697
saify <saifuddin.ksyed@gmail.com>
2016-12-12 22:22:46 +0530

    Commit

--- 0004_AddNNumbers/Java/AddNNumbers.java
+++ 0004_AddNNumbers/Java/AddNNumbers.java
@@ -1,1 +1,25 @@
 /* Problem Statement: Addition of first 'n' numbers; where 'n' numbers is provided by the user */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddNNumbers
+{
+	public static void main(String []args) throws NumberFormatException, IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int user_input; 
+		int count = 0;
+		
+		System.out.println("Addition of First 'n' numbers!");
+		System.out.print("Enter 'until' number you want to add: ");
+		user_input = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i <= user_input; i++ )
+		{
+			count = count + i;
+		}
+		
+		System.out.println("Addition of first "+ user_input +" numbers is: "+count);
+	}
+}
