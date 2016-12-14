ba7079a2388a26e3609ef400626fb4c079375fa5
saify <saifylovezyou@users.noreply.github.com>
2016-12-14 16:13:18 +0530

    Commit

--- 0008_PrimeNumber/Java/PrimeNumber.java
+++ 0008_PrimeNumber/Java/PrimeNumber.java
@@ -15,24 +15,24 @@ public class PrimeNumber
 	         	System.out.print(" "+2);
 	    	}
 	 
-	    for(int count = 2; count <= temp;)
-	    {
-	    	for(int j = 2; j <= Math.sqrt(num); j++)
-	        {
-	    		if(num % j == 0)
-	            	{
-	               		status = 0;
-	               		break;
-	            	}
-	        }
-	        if(status != 0)
-	        {
-	        	System.out.print(" "+num);
-	        	count++;
+	    	for(int count = 2; count <= temp;)
+	    	{
+	    		for(int j = 2; j <= Math.sqrt(num); j++)
+	        	{
+	    			if(num % j == 0)
+	            		{
+	               			status = 0;
+	               			break;
+	            		}
+	        	}
+	        	
+			if(status != 0)
+	        	{
+	        		System.out.print(" "+num);
+	        		count++;
 	        }
 	        status = 1;
 	        num++;
-	        
 	    }
 	    System.out.println();
 	}
@@ -99,5 +99,4 @@ public class PrimeNumber
 		}
 		while(ans.toLowerCase().equals("y"));
 	}
-
 }
